6e1d9d9a0691ccd32aefc697bbd836858855725f
xsjens02 <simjen02@easv365.dk>
2024-05-21 13:54:30 +0200

    added db connnection

--- Booking_System/src/main/java/module-info.java
+++ Booking_System/src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.booking_system {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.example.booking_system to javafx.fxml;
     exports com.example.booking_system;
