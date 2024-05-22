59317958dde18d3cba8562316dbf5077a155abd1
Kaqnae <noahvi01@easv365.dk>
2024-05-22 09:31:48 +0200

    Created LoginDAO_impl class

---
+++ Booking_System/src/main/java/com/example/booking_system/Persistence/LoginDAO_Impl.java
@@ -0,0 +1,30 @@
+package com.example.booking_system.Persistence;
+
+import java.util.List;
+
+public class LoginDAO_Impl implements DAO{
+    @Override
+    public boolean add(Object entity) {
+        return false;
+    }
+
+    @Override
+    public Object read(int id) {
+        return null;
+    }
+
+    @Override
+    public List readAll() {
+        return null;
+    }
+
+    @Override
+    public boolean remove(int id) {
+        return false;
+    }
+
+    @Override
+    public boolean update(Object entity) {
+        return false;
+    }
+}
