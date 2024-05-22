ca7f5d3de6943c373e1a8f486bc6415a65bba45e
Ronnie <148753940+Nessumsa@users.noreply.github.com>
2024-05-22 11:50:34 +0200

    The path to configurationWindow and buttons to respective windows are implemented.

--- Booking_System/src/main/java/com/example/booking_system/Controller/ConfigurationWindowController.java
+++ Booking_System/src/main/java/com/example/booking_system/Controller/ConfigurationWindowController.java
@@ -1,4 +1,101 @@
 package com.example.booking_system.Controller;
 
-public class ConfigurationWindowController {
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ConfigurationWindowController implements Initializable {
+
+    @FXML
+    VBox VBox;
+    @FXML
+    Button newMeetingRoomButton;
+    @FXML
+    Button newCateringOptionButton;
+    @FXML
+    Button adjustOpeningHoursAndTimeSlotsButton;
+    @FXML
+    Button returnButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        setWindowSize();
+    }
+    @FXML
+    private void setWindowSize(){
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        VBox.setMinHeight(primaryScreenBounds.getHeight()/2);
+        VBox.setMinWidth(primaryScreenBounds.getHeight()/2);
+        VBox.setMaxWidth(primaryScreenBounds.getWidth()/2);
+        adjustOpeningHoursAndTimeSlotsButton.setMinWidth(VBox.getMinWidth()-30);
+        newMeetingRoomButton.setMinWidth(adjustOpeningHoursAndTimeSlotsButton.getMinWidth());
+        newCateringOptionButton.setMinWidth(adjustOpeningHoursAndTimeSlotsButton.getMinWidth());
+        returnButton.setMinWidth(adjustOpeningHoursAndTimeSlotsButton.getMinWidth()/2);
+    }
+
+    @FXML
+    private void onNewMeetingRoomButtonClick(){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/booking_system/NewMeetingRoom.fxml"));
+            Parent root = loader.load();
+
+            Stage newMeetingRoomStage = new Stage();
+            newMeetingRoomStage.initModality(Modality.APPLICATION_MODAL);
+            newMeetingRoomStage.setTitle("Tilføj mødelokale");
+            newMeetingRoomStage.setScene(new Scene(root));
+            newMeetingRoomStage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void onNewCateringOptionButtonClick(){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/booking_system/NewCateringOption.fxml"));
+            Parent root = loader.load();
+
+            Stage newCateringOptionStage = new Stage();
+            newCateringOptionStage.initModality(Modality.APPLICATION_MODAL);
+            newCateringOptionStage.setTitle("Tilføj forplejning");
+            newCateringOptionStage.setScene(new Scene(root));
+            newCateringOptionStage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void onAdjustOpeningHoursAndTimeSlotsButtonClick(){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/booking_system/OpeningHoursAndTimeSlotsWindow.fxml"));
+            Parent root = loader.load();
+
+            Stage openingHoursAndTimeSlotsStage = new Stage();
+            openingHoursAndTimeSlotsStage.initModality(Modality.APPLICATION_MODAL);
+            openingHoursAndTimeSlotsStage.setTitle("Tilpas åbningstider og tidsintervaller");
+            openingHoursAndTimeSlotsStage.setScene(new Scene(root));
+            openingHoursAndTimeSlotsStage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void onReturnButtonClick(){
+        Stage stage = (Stage) returnButton.getScene().getWindow();
+        stage.close();
+    }
 }
