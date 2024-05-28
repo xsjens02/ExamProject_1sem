a8ae0f73508a7de2c3a71ab03d7f461866fd98b6
Noah <noahvi01@easv365.dk>
2024-05-28 21:52:55 +0200

    Statistics being implemented

--- Booking_System/src/main/java/com/example/booking_system/Controller/StatisticScreenController.java
+++ Booking_System/src/main/java/com/example/booking_system/Controller/StatisticScreenController.java
@@ -1,4 +1,26 @@
 package com.example.booking_system.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+
 public class StatisticScreenController {
+
+
+    public DatePicker dpStartdate;
+    public DatePicker dpEnddate;
+
+
+    public void getDates(){
+        dpStartdate.getValue();
+        dpEnddate.getValue();
+    }
+
+
+    public void onDownloadButtonClick(ActionEvent actionEvent) {
+
+
+
+
+
+    }
 }
--- Booking_System/src/main/java/com/example/booking_system/Model/Statistics.java
+++ Booking_System/src/main/java/com/example/booking_system/Model/Statistics.java
@@ -1,4 +1,30 @@
 package com.example.booking_system.Model;
 
+import com.example.booking_system.Controller.StatisticScreenController;
+import com.example.booking_system.Persistence.BookingDAO;
+import com.example.booking_system.Persistence.BookingDAO_Impl;
+
+import java.util.List;
+
 public class Statistics {
+
+    StatisticScreenController statisticController;
+
+    BookingDAO bookingDAO = new BookingDAO_Impl();
+
+    List<Booking> bookingList;
+
+    public void getBookings(){
+        for(MeetingRoom mr : SystemManager.getInstance().getInstitution().getMeetingRoomList()){
+            List<Booking> bookings = bookingDAO.readAllBookingsInPeriod(mr.getInstitutionID(), );
+            if(bookings != null){
+                for(Booking booking : bookings){
+                    bookingList.add(booking);
+                }
+            }
+        }
+    }
+
+
+
 }
